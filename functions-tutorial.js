3d2c86f8cdd7dd21e726320928e23cbb694be4c1
Junior Sarpong <lordxxiii@icloud.com>
2023-05-09 23:49:30 -0400

    completed Javascript Fundamentals Part 3 completed assignment & knowledge check

--- functions-tutorial.js
+++ functions-tutorial.js
@@ -935,3 +935,159 @@ function (name) {
     statement. So, all the examples A, B, and C can be used as function expressions, depending on how they are defined within a statement.
 */
 
+/*
+  JAVASCRIPT CALL STACK:
+    The call stack in JavaScript is like a to-do list for the computer. It keeps track of the order of functions that need to be completed. For example, if we have a function that 
+    calls other functions inside of it, the call stack will make sure those functions are completed before returning to the original function. The call stack also helps manage the 
+    different types of functions that are being executed. There are two types: GLOBAL FUNCTIONS, which are available to the entire program, and LOCAL FUNCTIONS, which are only 
+    accessible within a specific function. Overall, the call stack is a way for the JavaScript engine to keep track of where it is in the code and what needs to be done next. 
+    
+    The call stack works based on the LIFO principle i.e., last-in-first-out. When you execute a script, the JavaScript engine creates a global execution context and pushes it on 
+    top of the call stack. Whenever a function is called, the JavaScript engine creates a function execution context for the function, pushes it on top of the call stack, and starts 
+    executing the function. If a function calls another function, the JavaScript engine creates a new function execution context for the function being called and pushes it on top of 
+    the call tack. When the current function completes, the JavaScript engine pops it off the call stack and resumes the execution where it left off. The script will stop when the 
+    call stack is empty. JavaScript call stack example - Let’s start with the following example below:
+*/
+function add(a, b) {
+  return a + b;
+}
+
+function average(a, b) {
+  return add(a, b) / 2;
+}
+
+let x = average(10, 20);
+
+//  When the JavaScript engine executes this script, it places the global execution context (denoted by main() or global() function on the call stack.
+
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|             |
+|             |
+|    (MAIN)   |
+
+*/
+
+/*  
+  The global execution context enters the creation phase and moves to the execution phase. The JavaScript engine executes the call to the average(10, 20) function and creates 
+  a function execution context for the average() function and pushes it on top of the call stack:
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|             |
+|  (AVERAGE)  |
+|   (MAIN)    |
+
+*/
+
+/*
+  The JavaScript engine starts executing the average() since because the average() function is on the top of the call stack. The average() function calls add() function. At this 
+  point, the JavaScript engine creates another function execution context for the add() function and places it on the top of the call stack:
+*/
+
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|    (ADD)    |
+|  (AVERAGE)  |
+|   (MAIN)    |
+
+*/
+
+//  JavaScript engine executes the add() function and pops it off the call stack:
+
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|             |
+|  (AVERAGE)  |
+|   (MAIN)    |
+
+*/
+
+
+//  At this point, the average() function is on the top of the call stack, the JavaScript engine executes and pops it off the call stack.
+
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|             |
+|             |
+|   (MAIN)    |
+
+*/
+
+//  Now, the call stack is empty so the script stops executing:
+
+/*
+
+  CALL STACK
+|             |
+|             |
+|             |
+|             |
+|             |
+|             |
+
+*/
+
+/*
+  STACK OVERFLOW:
+    The call stack has a fixed size, depending on the implementation of the host environment, either the web browser or Node.js. If the number of execution contexts exceeds the size 
+    of the stack, a stack overflow error will occur. For example, when you execute a recursive function that has no exit condition, the JavaScript engine will issue a stack overflow 
+    error:
+*/
+function fn() {
+  fn();
+}
+
+fn(); // stack overflow
+
+/*
+  ASYNCHRONOUS JAVASCRIPT:
+    JavaScript is a single-threaded programming language. This means that the JavaScript engine has only one call stack. Therefore, it only can do one thing at a time. When executing 
+    a script, the JavaScript engine executes code from top to bottom, line by line. In other words, it is synchronous. Asynchronous means the JavaScript engine can execute other tasks 
+    while waiting for another task to be completed. For example, the JavaScript engine can:
+
+    A)  Request for data from a remote server.
+    B)  Display a spinner
+    C)  When the data is available, display it on the webpage.
+
+    To do this, the JavaScript engine uses an event loop, which will be covered in the following tutorial.
+*/
+
+/*
+  Summary
+    - JavaScript engine uses a call stack to manage execution contexts.
+    - The call stack uses the stack data structure that works based on the LIFO (last-in-first-out) principle.
+*/
+
+/*  
+
+JUNIORS CHAT:
+    Q:  What is a runtime environment?
+
+    A:  Think of a runtime environment like a stage where a play is performed. Just like a stage provides everything that actors need to perform, a runtime environment provides 
+        everything that code needs to run. A runtime environment includes a set of tools, libraries, and resources that are needed to execute a piece of code. It's like a virtual 
+        space that provides a platform for code to run on. Just like a stage has props, lighting, and sound, a runtime environment provides access to things like the internet, file 
+        systems, and other resources that are needed for the code to run. Different programming languages have different runtime environments. For example, JavaScript code runs in 
+        a browser's runtime environment, while Python code runs in a Python interpreter's runtime environment. Node.js is a JavaScript runtime environment that allows JavaScript to 
+        be executed on a computer or server outside of a browser.    
+
+*/
